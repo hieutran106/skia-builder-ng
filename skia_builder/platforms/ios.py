ce2efdab0444e1e2bef2c299932d45e3463f3665
Dexer <73297572+DexerBR@users.noreply.github.com>
2025-02-10 01:05:10 -0300

    fix host env setup for ios/iossimulator (#13)

--- skia_builder/platforms/ios.py
+++ skia_builder/platforms/ios.py
@@ -1,11 +1,16 @@
 from skia_builder.platforms.common import CommonSubPlatformManager, SubPlatform
+from skia_builder.platforms.macos import MacOSPlatformManager
 
 
 class IOSPlatformManager(CommonSubPlatformManager):
+    @staticmethod
+    def _setup_env_host_macos(skip_llvm_instalation):
+        MacOSPlatformManager.setup_env(skip_llvm_instalation)
+
     HOST_PLATFORMS_ENV_SETUP = {
         "Linux": None,
         "Windows": None,
-        "macOS": CommonSubPlatformManager.noop,
+        "macOS": _setup_env_host_macos,
     }
     TARGET_PLATFORM = SubPlatform.IOS
     HOST_PLATFORM = TARGET_PLATFORM.host_platform
--- skia_builder/platforms/iossimulator.py
+++ skia_builder/platforms/iossimulator.py
@@ -1,11 +1,16 @@
 from skia_builder.platforms.common import CommonSubPlatformManager, SubPlatform
+from skia_builder.platforms.macos import MacOSPlatformManager
 
 
 class IOSSimulatorPlatformManager(CommonSubPlatformManager):
+    @staticmethod
+    def _setup_env_host_macos(skip_llvm_instalation):
+        MacOSPlatformManager.setup_env(skip_llvm_instalation)
+
     HOST_PLATFORMS_ENV_SETUP = {
         "Linux": None,
         "Windows": None,
-        "macOS": CommonSubPlatformManager.noop,
+        "macOS": _setup_env_host_macos,
     }
     TARGET_PLATFORM = SubPlatform.IOS_SIMULATOR
     HOST_PLATFORM = TARGET_PLATFORM.host_platform
