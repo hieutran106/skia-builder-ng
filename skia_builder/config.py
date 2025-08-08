3ef708271f8a53e3440871b4e578526f4b16b043
Dexer <73297572+DexerBR@users.noreply.github.com>
2025-08-08 11:52:14 -0300

    Fix Skia linking error on Linux x64 with PIC flags and disable `skia_use_system_freetype2` (#32)
    * add "-fPIC" flag to linux x64 builds
    
    * Update config.py
    
    Update config.py
    
    * skia_use_system_freetype2

--- skia_builder/config.py
+++ skia_builder/config.py
@@ -141,6 +141,15 @@ platform_specific_flags = {
     "linux-x64": {
         **linux_base_flags,
         "target_cpu": "x86_64",
+        "skia_use_system_freetype2": False,
+        "extra_cflags": [
+            *linux_base_flags["extra_cflags"],
+            "-fPIC",
+        ],
+        "extra_cflags_cc": [
+            *linux_base_flags["extra_cflags_cc"],
+            "-fPIC",
+        ],
     },
     "linux-arm64": {
         **linux_base_flags,
